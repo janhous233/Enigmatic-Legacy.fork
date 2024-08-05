50f0187ba47dfa096bf2db70dffef5cdd9fa015b
Aizistral <admin@aizistral.com>
2024-08-05 20:46:08 +0200

    Change Unholy Grail drinking sound

--- src/main/java/com/aizistral/enigmaticlegacy/items/UnholyGrail.java
+++ src/main/java/com/aizistral/enigmaticlegacy/items/UnholyGrail.java
@@ -15,6 +15,8 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -23,11 +25,7 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.UseAnim;
-import net.minecraft.world.item.Vanishable;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -68,11 +66,11 @@ public class UnholyGrail extends ItemBase implements Vanishable {
 
 				UseUnholyGrailTrigger.INSTANCE.trigger((ServerPlayer) player, false);
 			} else {
-				player.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 1000/2, 2, false, true));
-				player.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 1600/2, 1, false, true));
-				player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 2400/2, 1, false, true));
-				player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 2000/2, 1, false, true));
-				player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 2400/2, 0, false, true));
+				player.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 1000 / 2, 2, false, true));
+				player.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 1600 / 2, 1, false, true));
+				player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 2400 / 2, 1, false, true));
+				player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 2000 / 2, 1, false, true));
+				player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 2400 / 2, 0, false, true));
 				//player.addEffect(new MobEffectInstance(MobEffects.HEAL, 1, 0, false, true));
 
 				UseUnholyGrailTrigger.INSTANCE.trigger((ServerPlayer) player, true);
@@ -82,6 +80,14 @@ public class UnholyGrail extends ItemBase implements Vanishable {
 		player.awardStat(Stats.ITEM_USED.get(this));
 
 		return stack;
+	}
+
+	public SoundEvent getDrinkingSound() {
+		return SoundEvents.HONEY_DRINK;
+	}
+
+	public SoundEvent getEatingSound() {
+		return SoundEvents.HONEY_DRINK;
 	}
 
 	@Override
